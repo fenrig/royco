c5f77c3b32ea7bc02a6bb8037e8e202208108cb8
Roy <Roy@192.168.0.247>
2016-01-05 16:50:51 +0100

    werkInterestVoetenBij werkt

--- JEEProject/JEEProject-ejb/src/java/beans/RemoteBean.java
+++ JEEProject/JEEProject-ejb/src/java/beans/RemoteBean.java
@@ -5,6 +5,7 @@
  */
 package beans;
 
+import java.util.*;
 import javax.ejb.*;
 
 /**
@@ -18,9 +19,32 @@ public class RemoteBean implements RemoteBeanRemote
     private localStatelessBeanLocal localStatelessBean;
 
     @Override
-    public String test()
+    public String werkInterestVoetenBij()
     {
-        return Integer.toString(localStatelessBean.getAllLeningen().size());
-    }
+        List<Lening> leningen = localStatelessBean.getAllLeningen();
+        StringBuilder result = new StringBuilder();
 
+        for (Lening lening : leningen)
+        {
+            double saldo = lening.getSaldo();
+            double interest = lening.getInterest();
+            double nieuw = saldo * (1 + interest);
+            
+            result.append("lening van ");
+            result.append(lening.getKnr().getPnr().getPvoornaam());
+            result.append(" ");
+            result.append(lening.getKnr().getPnr().getPachternaam());
+            result.append("; huidig saldo: ");
+            result.append(saldo);
+            result.append("; interest: ");
+            result.append(interest);
+            result.append("; nieuw saldo: ");
+            result.append(nieuw);
+            result.append("\n");
+            
+            lening.setSaldo(nieuw);
+        }
+
+        return result.toString();
+    }
 }
